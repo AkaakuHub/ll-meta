ebdb80db1f82773968924845a7303dd9b3099dec
AkaakuHub <akaaku2021media01@gmail.com>
2026-03-19 01:28:53 +0900

    chore: apply WPF UI

--- OpenKikaiSan.App/MainWindow.xaml.cs
+++ OpenKikaiSan.App/MainWindow.xaml.cs
@@ -1,14 +1,13 @@
 using System.Windows;
+using Wpf.Ui.Appearance;
 
 namespace OpenKikaiSan.App;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        SystemThemeWatcher.Watch(this);
     }
 }
