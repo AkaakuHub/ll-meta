485cc783cbdf1e09c4e4573aed67da5663b943e4
AkaakuHub <akaaku2021media01@gmail.com>
2026-03-20 18:40:58 +0900

    feat: apply new WPF UI

--- OpenKikaiSan.App/MainWindow.xaml.cs
+++ OpenKikaiSan.App/MainWindow.xaml.cs
@@ -1,13 +1,19 @@
-using System.Windows;
+using System.Windows.Media;
 using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
 
 namespace OpenKikaiSan.App;
 
-public partial class MainWindow : Window
+public partial class MainWindow : FluentWindow
 {
     public MainWindow()
     {
         InitializeComponent();
-        SystemThemeWatcher.Watch(this);
+        ApplicationAccentColorManager.Apply(
+            Color.FromRgb(0x68, 0xBE, 0x8D),
+            ApplicationTheme.Light,
+            false,
+            false
+        );
     }
 }
